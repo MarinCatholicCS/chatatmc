d8333aa2e538e8d6241696e31870b6609e2a885d
razzyshmazzy <razzyshmazzy@gmail.com>
2026-01-29 12:49:24 -0800

    switched sum stuff

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ function checkAccess(event) {
     let answer = document.getElementById("inputer").value;
 
     if (answer && answer.toLowerCase() === "adib") {
-        document.getElementById("hiddenContent").style.display = "block";
-        let auths = document.getElementsByClassName("auth");
-        auths.forEach(element => {
-            element.remove();
-        });
+        window.location.replace("chat.html")
     } else {
         document.body.innerHTML = "<h1>Access Denied</h1>";
     }
