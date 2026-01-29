e2f844ea57f55f94ddcba4706e99db2319df2ffa
razzyshmazzy <razzyshmazzy@gmail.com>
2026-01-29 13:04:36 -0800

    s

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function checkAccess(event) {
     let answer = document.getElementById("inputer").value;
 
     if (answer && answer.toLowerCase() === "adib") {
+        console.log("running redirection");
         window.location.href = "chat/chat.html";
+        console.log("redirecting");
     } else {
         document.body.innerHTML = "<h1>Access Denied</h1>";
     }
