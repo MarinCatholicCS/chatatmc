acd49a384a998791dfc347d76467b0033d7f398f
razzyshmazzy <razzyshmazzy@gmail.com>
2026-01-29 10:06:22 -0800

    keydown space added

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const answer = prompt("What his name is?");
+//const answer = prompt("What his name is?");
 
-if (answer && answer.toLowerCase() === "correctanswer") {
-    document.getElementById("content").style.display = "block";
-    document.getElementById("MoAdib").style.display = "none";
-} else {
-    document.body.innerHTML = "<h1>Access Denied</h1>";
+function checkAcess() {
+    let answer = document.getElementById("inputer").innerHTML;
+
+    if (answer && answer.toLowerCase() === "adib") {
+        document.getElementById("content").style.display = "block";
+        document.getElementById("MoAdib").style.display = "none";
+    } else {
+        document.body.innerHTML = "<h1>Access Denied</h1>";
+    }
 }
